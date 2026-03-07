e2b719fd31efa05fc0a60b0633f5d4fc978c3e03
recall <aldirifaldi2008.com@gmail.com>
2026-03-07 19:58:37 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,13 +2,23 @@ const CACHE_NAME = 'wordfinder-v1';
 const ASSETS = [
   './',
   './index.html',
-  './kamus.txt' // Masukkan semua nama file kamus Anda di sini
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;600&display=swap'
 ];
 
+// Install Service Worker
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fetch Assets dari Cache
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
